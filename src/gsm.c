5eff8fd1444679053bcaafe9118d30526721212c
romeudreadcore <romeudreadcore@gmail.com>
2026-08-16 11:44:25 -0300

    Test wider DW for 720p Wide

--- src/gsm.c
+++ src/gsm.c
@@ -103,7 +103,7 @@ void PrepareGSM(char *cmdline, struct GsmConfig_t *config)
         {GS_NONINTERLACED, GS_MODE_VGA_1280_60, GS_FRAME, makeDISPLAY(1023, 1279, 1,   1,   40,  350), makeSYNCV(3,  1024, 0,    38,  0,   1)},
         {GS_NONINTERLACED, GS_MODE_VGA_1280_75, GS_FRAME, makeDISPLAY(1023, 1279, 1,   1,   40,  350), makeSYNCV(3,  1024, 0,    38,  0,   1)},
         {GS_NONINTERLACED, GS_MODE_DTV_480P,    GS_FRAME, makeDISPLAY(479,  1535, 0,   1,   35,  200), makeSYNCV(6,  483,  3072, 30,  0,   6)},
-        {GS_NONINTERLACED, GS_MODE_DTV_720P,    GS_FRAME, makeDISPLAY(719,  1279, 2,   2,   24,  302), makeSYNCV(5,  720,  0,    20,  0,   5)},}; //ends predef_vmode definition
+        {GS_NONINTERLACED, GS_MODE_DTV_720P,    GS_FRAME, makeDISPLAY(719,  1439, 1,   1,   24,  302), makeSYNCV(5,  720,  0,    20,  0,   5)},}; //ends predef_vmode definition
     // clang-format on
     int k576p_fix, kGsDxDyOffsetSupported, fd, FIELD_fix;
     char romver[16], romverNum[5], *pROMDate;
